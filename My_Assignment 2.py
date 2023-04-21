b3b76eda688f3008f0e65aae76dc7313bc4db5e5
Joh-Juh-Chi <131270804+Joh-Juh-Chi@users.noreply.github.com>
2023-04-21 03:08:12 +0200

    SN Lab test
    Insisde is my first work for the given test

---
+++ My_Assignment 2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## ___Objectives:___
+
+# # In this notebook, Data science Tools and Ecosystem is summarized.
+# 
+# Some of the popular languages that Data Scientists use are:
+#     
+# |Python
+# -----------
+# |SQL
+# ----------
+# |R
+# ---------- 
+# 
+# 
+# 
+# 
+# 
+# 
+
+# 
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Numpy
+# 3. Tensorflow
+# 4. Matplotlib
+# 5. Scrapy
+# 
+
+# 
+# # Data Science Tools
+#    
+# 
+# |Python|
+# |Jupyter Notebook|
+# |Google Colab|
+# 
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[11]:
+
+
+### This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+
+# In[13]:
+
+
+## This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# ## Author
+# John
+
+# In[ ]:
+
+
+
+
+
+
+
